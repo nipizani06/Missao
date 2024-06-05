c655f995f1d8d1df8e06fca06bd99bebea3ba534
ni.pizani <106701222+nipizani06@users.noreply.github.com>
2024-06-05 14:35:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,38 +6,42 @@ const textoResultado = document.querySelector(".caixa-resultado");
 
 const perguntas = [
   {
-    enunciado: "Pergunta 1",
+    enunciado: "As unhas, como qualquer parte do corpo, não são isentas de doenças. Assinale a alternativa que indica o tipo de organismo que se 
+      alimenta de queratina e causa a onicomicose, uma infecção relativamente comum nas unhas:,
     alternativas: [
-      "Alternativa 1",
-      "Alternativa 2"
+      "Bactérias.",
+      "Fungos."
       ]
   },
   {
-    enunciado: "Pergunta 2",
+    enunciado: "Marque a alternativa que apresenta uma informação incorreta sobre as unhas:",
     alternativas: [
-      "Alternativa 1",
-      "Alternativa 2"
+      "As unhas das mãos crescem mais rapidamente que as dos pés.",
+      "O prato ungueal é uma estrutura de coloração rosada por causa da presença de pigmentos."
        ]
   },
   {
-    enunciado: "Pergunta 3",
+    enunciado: "Ao estudar a unha, vemos que ela é formada por várias partes. Uma dessas partes é mais branca e opaca que o restante da unha e forma 
+      um semicírculo característico. Entre as alternativas a seguir, qual representa o nome dessa porção da unha?",
     alternativas: [
-      "Alternativa 1",
-      "Alternativa 2"
+      "Lúnula.",
+      "Hiponíquio."
        ]
   },
   {
-    enunciado: "Pergunta 4",
+    enunciado: "As unhas são constituídas principalmente por queratina e são formadas por quatro epitélios básicos. Analise as alternativas e marque a região onde é encontrado o epitélio germinativo.",
     alternativas: [
-      "Alternativa 1",
-      "Alternativa 2"
+      "Matriz ungueal.",
+      "Leito ungueal."
        ]
   },
   {
-    enunciado: "Pergunta 5",
+
+    enunciado: "As unhas apresentam algumas partes básicas. Analise as alternativas abaixo e marque aquela que indica o nome da porção que tem a aparência de uma meia-lua, 
+      de coloração esbranquiçada, localizada na parte proximal da lâmina ungueal.",
     alternativas: [
-      "Alternativa 1",
-      "Alternativa 2"
+      "Eponíquio.",
+      " Leito ungueal."
        ]
   }
 ];
