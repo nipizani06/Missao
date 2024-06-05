0d7b3d29914bd803e720222309c7c24dfa02331a
ni.pizani <106701222+nipizani06@users.noreply.github.com>
2024-06-05 14:48:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -52,6 +52,8 @@ let perguntaAtual;
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas();
 }
+  function mostraAlternativas() {}
 
 mostraPergunta();
