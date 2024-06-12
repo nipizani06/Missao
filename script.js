0a04efba74d6d0b13c624553a26e73ad8348e540
ni.pizani <106701222+nipizani06@users.noreply.github.com>
2024-06-12 15:15:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ const textoResultado = document.querySelector(".caixa-resultado");
 
 const perguntas = [
   {
-    enunciado: 
-      "As unhas, como qualquer parte do corpo, não são isentas de doenças. Assinale a alternativa que indica o tipo de organismo que se 
+    enunciado: "As unhas, como qualquer parte do corpo, não são isentas de doenças. Assinale a alternativa que indica o tipo de organismo que se 
       alimenta de queratina e causa a onicomicose, uma infecção relativamente comum nas unhas:,
     alternativas: [
       {
@@ -16,8 +15,7 @@ const perguntas = [
       ]
   },
   {
-    enunciado: 
-      "Marque a alternativa que apresenta uma informação incorreta sobre as unhas:",
+    enunciado: "Marque a alternativa que apresenta uma informação incorreta sobre as unhas:",
     alternativas: [
       {
       texto: "As unhas das mãos crescem mais rapidamente que as dos pés.",
@@ -25,8 +23,7 @@ const perguntas = [
        ]
   },
   {
-    enunciado: 
-      "Ao estudar a unha, vemos que ela é formada por várias partes. Uma dessas partes é mais branca e opaca que o restante da unha e forma 
+    enunciado: "Ao estudar a unha, vemos que ela é formada por várias partes. Uma dessas partes é mais branca e opaca que o restante da unha e forma 
       um semicírculo característico. Entre as alternativas a seguir, qual representa o nome dessa porção da unha?",
     alternativas: [
       {
@@ -35,8 +32,7 @@ const perguntas = [
        ]
   },
   {
-    enunciado: 
-      "As unhas são constituídas principalmente por queratina e são formadas por quatro epitélios básicos. Analise as alternativas e marque a região onde é encontrado o epitélio germinativo.",
+    enunciado: "As unhas são constituídas principalmente por queratina e são formadas por quatro epitélios básicos. Analise as alternativas e marque a região onde é encontrado o epitélio germinativo.",
     alternativas: [
       {
       texto: "Matriz ungueal.",
@@ -45,8 +41,7 @@ const perguntas = [
   },
   {
 
-    enunciado: 
-      "As unhas apresentam algumas partes básicas. Analise as alternativas abaixo e marque aquela que indica o nome da porção que tem a aparência de uma meia-lua, 
+    enunciado: "As unhas apresentam algumas partes básicas. Analise as alternativas abaixo e marque aquela que indica o nome da porção que tem a aparência de uma meia-lua, 
       de coloração esbranquiçada, localizada na parte proximal da lâmina ungueal.",
     alternativas: [
       { 
