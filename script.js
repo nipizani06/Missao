3c0fed200c4dc1584bd2c8440c8c170f449812aa
ni.pizani <106701222+nipizani06@users.noreply.github.com>
2024-06-12 14:37:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,42 +6,52 @@ const textoResultado = document.querySelector(".caixa-resultado");
 
 const perguntas = [
   {
-    enunciado: "As unhas, como qualquer parte do corpo, não são isentas de doenças. Assinale a alternativa que indica o tipo de organismo que se 
+    enunciado: 
+      "As unhas, como qualquer parte do corpo, não são isentas de doenças. Assinale a alternativa que indica o tipo de organismo que se 
       alimenta de queratina e causa a onicomicose, uma infecção relativamente comum nas unhas:,
     alternativas: [
-      "Bactérias.",
-      "Fungos."
+      {
+      texto: "Bactérias.",
+      afirmação: "Fungos."
       ]
   },
   {
-    enunciado: "Marque a alternativa que apresenta uma informação incorreta sobre as unhas:",
+    enunciado: 
+      "Marque a alternativa que apresenta uma informação incorreta sobre as unhas:",
     alternativas: [
-      "As unhas das mãos crescem mais rapidamente que as dos pés.",
-      "O prato ungueal é uma estrutura de coloração rosada por causa da presença de pigmentos."
+      {
+      texto: "As unhas das mãos crescem mais rapidamente que as dos pés.",
+      afirmação: "O prato ungueal é uma estrutura de coloração rosada por causa da presença de pigmentos."
        ]
   },
   {
-    enunciado: "Ao estudar a unha, vemos que ela é formada por várias partes. Uma dessas partes é mais branca e opaca que o restante da unha e forma 
+    enunciado: 
+      "Ao estudar a unha, vemos que ela é formada por várias partes. Uma dessas partes é mais branca e opaca que o restante da unha e forma 
       um semicírculo característico. Entre as alternativas a seguir, qual representa o nome dessa porção da unha?",
     alternativas: [
-      "Lúnula.",
-      "Hiponíquio."
+      {
+      texto: "Lúnula.",
+      afirmação: "Hiponíquio."
        ]
   },
   {
-    enunciado: "As unhas são constituídas principalmente por queratina e são formadas por quatro epitélios básicos. Analise as alternativas e marque a região onde é encontrado o epitélio germinativo.",
+    enunciado: 
+      "As unhas são constituídas principalmente por queratina e são formadas por quatro epitélios básicos. Analise as alternativas e marque a região onde é encontrado o epitélio germinativo.",
     alternativas: [
-      "Matriz ungueal.",
-      "Leito ungueal."
+      {
+      texto: "Matriz ungueal.",
+      afirmação: "Leito ungueal."
        ]
   },
   {
 
-    enunciado: "As unhas apresentam algumas partes básicas. Analise as alternativas abaixo e marque aquela que indica o nome da porção que tem a aparência de uma meia-lua, 
+    enunciado: 
+      "As unhas apresentam algumas partes básicas. Analise as alternativas abaixo e marque aquela que indica o nome da porção que tem a aparência de uma meia-lua, 
       de coloração esbranquiçada, localizada na parte proximal da lâmina ungueal.",
     alternativas: [
-      "Eponíquio.",
-      " Leito ungueal."
+      { 
+      texto: "Eponíquio.",
+      afirmação: " Leito ungueal."
        ]
   }
 ];
