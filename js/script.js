902cad3865644340bbe579c6b0ca48fec3fbf9a7
ni.pizani <106701222+nipizani06@users.noreply.github.com>
2024-08-14 14:48:45 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,7 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
